e8c2959ed8ba69e46962e7ae379956be7f25e8bf
Shivam256 <shivamgavandi256@gmail.com>
2020-07-18 22:24:36 +0530

    adding header component to the app to navigate to shop page and contacts

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import HomePage from './pages/homapage/homepage.components';
 import ShopPage from './pages/shop/shop.components';
+import Header from './components/header/header.components';
 
 function App() {
   return (
     <div >
+    <Header/>
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
